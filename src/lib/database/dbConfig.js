9fc7c0d61505beaa1e4277da036c269ded1419b0
vivek sagar <vivek54540@gmail.com>
2024-08-13 19:28:07 +0530

    task done v5

--- src/lib/database/dbConfig.js
+++ src/lib/database/dbConfig.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 // new db config
-export async function connect() {
+export default async function connect() {
   try {
     if (mongoose.connections[0].readyState) {
       console.log('Already connected to MongoDB');
